777dae4ea4d8a5b758d4072d06bba6d11f92df33
pedromello <pedro.renato.mello@gmail.com>
2026-01-06 20:07:58 -0300

    Adding Prisma module to next js and database.ts

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,4 +1,8 @@
-function status(req: any, res: any) {
+import { prisma } from "../../../../infra/database";
+
+async function status(req: any, res: any) {
+  await prisma.$connect();
+  await prisma.$disconnect();
   res.status(200).json({ status: "Index API is running smoothly." });
 }
 
