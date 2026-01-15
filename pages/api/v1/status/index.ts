1f9a0d2b181b695e69b954b35a8b04c69aa24181
Pedro Mello <pedro.renato.mello@gmail.com>
2026-01-15 00:24:48 -0300

    /status with databse status

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,10 +1,27 @@
 import { prisma } from "infra/database";
 
 async function status(req: any, res: any) {
-  await prisma.$connect();
-  await prisma.$queryRaw`SELECT 1+1;`;
-  await prisma.$disconnect();
-  res.status(200).json({ status: "Index API is running smoothly." });
+  const updatedAt = new Date().toISOString();
+
+  const dbVersionResult = await prisma.$queryRaw<{ server_version: string }>`SHOW server_version;`;
+  const dbVersion = dbVersionResult[0].server_version;
+
+  const dbMaxConnectionsResult = await prisma.$queryRaw<{ max_connections: string }>`SHOW max_connections;`;
+  const dbMaxConnections = dbMaxConnectionsResult[0].max_connections;
+
+  const dbOpenConnectionsResult = await prisma.$queryRaw<{ count: bigint }[]>`SELECT COUNT(*) as count FROM pg_stat_activity WHERE datname = 'local_db';`;
+  const dbOpenConnections = Number(dbOpenConnectionsResult[0].count);
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: parseInt(dbMaxConnections),
+        open_connections: dbOpenConnections,
+      },
+    },
+  });
 }
 
 export default status;
