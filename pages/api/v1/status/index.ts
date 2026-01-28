1d06e548067de11757ab84063e9ade3e368f071c
Pedro Renato Mello <pedro.renato.mello@gmail.com>
2026-01-28 02:36:03 +0000

    database name as param

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,15 +1,24 @@
+import { Prisma } from "generated/prisma/client";
 import { prisma } from "infra/database";
 
 async function status(req: any, res: any) {
   const updatedAt = new Date().toISOString();
 
-  const dbVersionResult = await prisma.$queryRaw<{ server_version: string }>`SHOW server_version;`;
+  const dbVersionResult = await prisma.$queryRaw<{
+    server_version: string;
+  }>`SHOW server_version;`;
   const dbVersion = dbVersionResult[0].server_version;
 
-  const dbMaxConnectionsResult = await prisma.$queryRaw<{ max_connections: string }>`SHOW max_connections;`;
+  const dbMaxConnectionsResult = await prisma.$queryRaw<{
+    max_connections: string;
+  }>`SHOW max_connections;`;
   const dbMaxConnections = dbMaxConnectionsResult[0].max_connections;
 
-  const dbOpenConnectionsResult = await prisma.$queryRaw<{ count: bigint }[]>`SELECT COUNT(*) as count FROM pg_stat_activity WHERE datname = 'local_db';`;
+  const databaseName = process.env.POSTGRES_DB;
+
+  const dbOpenConnectionsResult = await prisma.$queryRaw(
+    Prisma.sql`SELECT COUNT(*) as count FROM pg_stat_activity WHERE datname = ${databaseName};`,
+  );
   const dbOpenConnections = Number(dbOpenConnectionsResult[0].count);
 
   res.status(200).json({
