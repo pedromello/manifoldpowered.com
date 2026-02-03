a056237277c17604c9b37a106b6e49655a69f20c
pedromello <pedro.renato.mello@gmail.com>
2026-02-03 09:49:57 -0300

    small log

--- infra/database.ts
+++ infra/database.ts
@@ -4,6 +4,7 @@ import { PrismaClient } from "generated/prisma/client";
 
 const connectionString = `postgresql://${process.env["POSTGRES_USER"]}:${process.env["POSTGRES_PASSWORD"]}@${process.env["POSTGRES_HOST"]}:${process.env["POSTGRES_PORT"]}/${process.env["POSTGRES_DB"]}?schema=public`;
 
+console.log(connectionString);
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
