041b8a69e338ad1d6d60c894315dfaa13ddd8acd
Pedro Mello <pedro.renato.mello@gmail.com>
2026-02-24 10:32:07 -0300

    trigger vercel preview deploy

--- tests/integration/api/v1/status/get.test.ts
+++ tests/integration/api/v1/status/get.test.ts
@@ -7,7 +7,7 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(parsedUpdatedAt).toEqual(responseBody.updated_at);
 
-  // Test database version
+  // Test database version 
   expect(responseBody.dependencies.database.version).toEqual('16.0');
 
   // Test database max connections
