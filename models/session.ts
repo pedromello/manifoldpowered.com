91ee011f9ad99f59ff09ff2a97242734e8e37d60
pedromello <pedro.renato.mello@gmail.com>
2026-04-02 16:14:55 -0300

    feat: session read and renewal on sessions model

--- models/session.ts
+++ models/session.ts
@@ -1,4 +1,5 @@
 import { prisma } from "infra/database";
+import { UnauthorizedError } from "infra/errors";
 import crypto from "node:crypto";
 
 const EXPIRATION_IN_MILLISECONDS = 1000 * 60 * 60 * 24 * 30; // 30 days
@@ -17,6 +18,40 @@ async function create(userId: string) {
     return newSession;
 }
 
-const session = { create, EXPIRATION_IN_MILLISECONDS };
+async function renew(sessionId: string) {
+    const updatedSession = await prisma.session.update({
+        where: {
+            id: sessionId,
+        },
+        data: {
+            expires_at: new Date(Date.now() + EXPIRATION_IN_MILLISECONDS),
+        },
+    });
+    return updatedSession;
+}
+
+async function findOneValidByToken(token: string) {
+    const foundSession = await prisma.session.findUnique({
+        where: {
+            token: token,
+            expires_at: {
+                gt: new Date(),
+            },
+        },
+    });
+
+    if (!foundSession) {
+        throw new UnauthorizedError({
+            message: "User does not have a valid session",
+            action: "Check if user is logged in and try again",
+        });
+    }
+
+    return foundSession;
+}
+
+
+
+const session = { create, renew, findOneValidByToken, EXPIRATION_IN_MILLISECONDS };
 
 export default session;
