2299526968b1bfcf716cdfa4bafa3f19eb145a4b
pedromello <pedro.renato.mello@gmail.com>
2026-04-03 17:10:57 -0300

    feat: email model with integration tests

---
+++ infra/email.ts
@@ -0,0 +1,28 @@
+import nodemailer from "nodemailer";
+
+export interface MailOptions {
+  from: string;
+  to: string;
+  subject: string;
+  text: string;
+}
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_SMTP_HOST,
+  port: Number(process.env.EMAIL_SMTP_PORT),
+  auth: {
+    user: process.env.EMAIL_SMTP_USER,
+    pass: process.env.EMAIL_SMTP_PASSWORD,
+  },
+  secure: process.env.NODE_ENV === "production" ? true : false,
+});
+
+async function send(mailOptions: MailOptions) {
+  await transporter.sendMail(mailOptions);
+}
+
+const email = {
+  send,
+};
+
+export default email;
--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -4,8 +4,11 @@ import user from "models/user";
 import session from "models/session";
 import { faker } from "@faker-js/faker";
 
+const EMAIL_HTTP_URL = `http://${process.env.EMAIL_HTTP_HOST}:${process.env.EMAIL_HTTP_PORT}`;
+
 const waitForAllServices = async () => {
   await waitForWebServer();
+  await waitForEmailServer();
 
   async function waitForWebServer() {
     await retry(
@@ -13,6 +16,18 @@ const waitForAllServices = async () => {
         const response = await fetch("http://localhost:3000/api/v1/status");
         if (response.status !== 200) {
           throw new Error("Web server is not ready");
+        }
+      },
+      { retries: 100, maxTimeout: 1000 },
+    );
+  }
+
+  async function waitForEmailServer() {
+    await retry(
+      async () => {
+        const response = await fetch(`${EMAIL_HTTP_URL}`);
+        if (response.status !== 200) {
+          throw new Error("Email server is not ready");
         }
       },
       { retries: 100, maxTimeout: 1000 },
@@ -41,12 +56,37 @@ const createSession = async (userId) => {
   return session.create(userId);
 };
 
+// Emails
+const deleteAllEmails = async () => {
+  // There is no method to delete all emails through the transporter, so we use the HTTP API
+  await fetch(`${EMAIL_HTTP_URL}/messages`, {
+    method: "DELETE",
+  });
+};
+
+const getLastEmail = async () => {
+  const emailListResponse = await fetch(`${EMAIL_HTTP_URL}/messages`);
+  const emailList = await emailListResponse.json();
+
+  const lastEmail = emailList.pop();
+
+  const emailContentResponse = await fetch(
+    `${EMAIL_HTTP_URL}/messages/${lastEmail.id}.plain`,
+  );
+  const emailContent = await emailContentResponse.text();
+
+  lastEmail.text = emailContent;
+  return lastEmail;
+};
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
   clearDatabaseRows,
   createUser,
   createSession,
+  deleteAllEmails,
+  getLastEmail,
 };
 
 export default orchestrator;
